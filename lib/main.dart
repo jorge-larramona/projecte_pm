18541b81eccc464df2d7e59e4d6f610da0dad45c
jorge_larramona_pardina <jorge.larramona@estudiantat.upc.edu>
2025-12-02 19:32:49 +0100

    Proyecto con autenticacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:projecte_pm/pages/LandingPage.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+import 'app.dart';
+import 'firebase_options.dart';
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LandingPage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
+  runApp(const MyApp());
 }
